f69204c86969b19a846d92169b2cc9b5b2d1531e
ChokSheak Lau <choksheak@gmail.com>
2025-10-25 20:07:10 -0700

    Initial setup for npm package

---
+++ src/example.test.ts
@@ -0,0 +1,3 @@
+test("Example test", () => {
+  console.log(`Please delete this test after you have real tests.`);
+});
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// Unused file. To support full tree-shaking, please import from the individual
+// files directly.
+
+console.log(`Hello from Chok's React Utils`);
