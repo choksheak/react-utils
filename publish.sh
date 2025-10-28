b556e062810110c68fe20c24f6858549c77b733a
ChokSheak Lau <choksheak@gmail.com>
2025-10-28 09:53:27 -0700

    Try fix dist

--- publish.sh
+++ publish.sh
@@ -5,16 +5,19 @@ cd $(dirname "$0")
 echo CI
 npm run ci
 
-echo Generate types
-npm run gen-types
-
 echo Bump version
 npm run bump
-npm i
 
-echo Copy files
+echo Init dist
+npm i
 rm -rf dist
 mkdir dist
+
+echo Build
+npm run build
+npm run gen-types
+
+echo Copy files
 cp -r package.json LICENSE README.md src dist
 
 echo Remove test files
