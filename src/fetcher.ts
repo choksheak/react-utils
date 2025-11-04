dac2827b14f3ba1ec4a2659b2f2249f2c80d002a
ChokSheak Lau <choksheak@gmail.com>
2025-11-03 21:18:09 -0800

    Add docs for fetcher

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,14 +1,42 @@
+/**
+ * @packageDocumentation
+ *
+ * An alternate syntax to fetch() that allows function chaining to compose the
+ * entire fetch request. One can also compose partial fetcher objects to be
+ * reused in future requests.
+ *
+ * Examples:
+ * ```
+ *   const userFetcher = fetcher
+ *     .auth("Bearer TOPSECRET")
+ *     .content("application/json")
+ *     .url("/api/user");
+ *
+ *   const user = await userFetcher.get();
+ *
+ *   const updateResult = await userFetcher
+ *     .body({ id: user.id, name: "New name"})
+ *     .post();
+ * ```
+ */
+
 import wretch from "wretch";
 import { retry } from "wretch/middlewares";
 
-/** Name this as "fetcher" to make it easier to understand. */
-export const fetcher = wretch();
-
-/** Fetcher with default short retries. */
-export const fetcherWithRetries = wretch().middlewares([
+/**
+ * Fetcher with default short retries. Use this to make your queries more
+ * robust without any additional work.
+ */
+export const fetcher = wretch().middlewares([
   retry({
     maxAttempts: 3,
     delayTimer: 1000,
     retryOnNetworkError: true,
   }),
 ]);
+
+/**
+ * Name this as `simpleFetcher` to make it easier to understand than `wretch`.
+ * Use this if you don't want retries. Use `fetcher` if you want retries.
+ */
+export const simpleFetcher = wretch();
